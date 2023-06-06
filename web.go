93bcbf6d5d831a15e391218419e6f29685618d4b
hsmtkk <hsmtkk@gmail.com>
2023-06-06 14:53:03 +0900

    up

--- web.go
+++ web.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"cloud.google.com/go/firestore"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"google.golang.org/api/iterator"
@@ -40,7 +41,9 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", handler.List)
+	e.GET("/", handler.ListGet)
+	e.GET("/new", handler.NewGet)
+	e.POST("/new", handler.NewPost)
 
 	// Start server
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
@@ -55,11 +58,10 @@ func newHandler(projectID string) *handler {
 }
 
 type item struct {
-	ID   int    `json:"id" firestore:"id"`
 	Text string `json:"text" firestore:"text"`
 }
 
-func (h *handler) List(ectx echo.Context) error {
+func (h *handler) ListGet(ectx echo.Context) error {
 	ctx := ectx.Request().Context()
 	client, err := firestore.NewClient(ctx, h.projectID)
 	if err != nil {
@@ -82,4 +84,29 @@ func (h *handler) List(ectx echo.Context) error {
 		items = append(items, item)
 	}
 	return ectx.JSON(http.StatusOK, items)
+}
+
+func (h *handler) NewGet(ectx echo.Context) error {
+	html := `<form method="POST" action="/new">
+<input type="text" name="text">
+</form>`
+	return ectx.HTML(http.StatusOK, html)
+}
+
+func (h *handler) NewPost(ectx echo.Context) error {
+	ctx := ectx.Request().Context()
+	client, err := firestore.NewClient(ctx, h.projectID)
+	if err != nil {
+		return fmt.Errorf("failed to init firestore client: %w", err)
+	}
+	defer client.Close()
+	id, err := uuid.NewString()
+	if err != nil {
+		return fmt.Errorf("failed to generate new ID: %w", err)
+	}
+	text := ectx.FormValue("text")
+	if _, err := client.Collection(COLLECTION).Doc(id).Set(ctx, item{Text: text}); err != nil {
+		return fmt.Errorf("failed to set item: %w", err)
+	}
+	return ectx.Redirect(http.StatusSeeOther, "/")
 }
